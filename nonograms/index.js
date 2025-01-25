56eb6e10cf596e667156a1d781b83b804f2fb8a5
Natalia-1707 <nataliatsepoukhowa@gmail.com>
2025-01-25 17:27:30 +0300

    init: start making nonongrams, add index.html, style.css and index.js files

---
+++ nonograms/index.js
@@ -0,0 +1,17 @@
+// CSS //
+let styleLink = document.createElement('link');
+styleLink.rel = 'stylesheet';
+styleLink.href = 'style.css';
+document.head.appendChild(styleLink);
+
+// fonts //
+let fontLink = document.createElement('link');
+fontLink.rel = 'stylesheet';
+fontLink.href = 'https://fonts.googleapis.com/css2?family=Allura&family=Jost:ital,wght@0,100..900;1,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playwrite+AU+SA:wght@100..400&family=Reggae+One&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap';
+document.head.appendChild(fontLink);
+
+let fontAwesomeLink = document.createElement('link');
+fontAwesomeLink.rel = 'stylesheet';
+fontAwesomeLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css';
+document.head.appendChild(fontAwesomeLink);
+
